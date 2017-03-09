94b6823919a49e6347f562f29d2f5cd555a43327
Abhilash-Mandlekar <abhilash.mandlekar@gmail.com>
2017-03-09 18:48:58 +0530

    connected with dynamodb database.

--- app/src/main/java/com/varvet/barcodereadersample/MainActivity.java
+++ app/src/main/java/com/varvet/barcodereadersample/MainActivity.java
@@ -3,6 +3,7 @@ package com.varvet.barcodereadersample;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Point;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -13,7 +14,13 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.amazonaws.ClientConfiguration;
+import com.amazonaws.regions.Region;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
@@ -24,6 +31,11 @@ import com.varvet.barcodereadersample.barcode.BarcodeCaptureActivity;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.*;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.*;
+import com.amazonaws.services.dynamodbv2.model.*;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
@@ -34,10 +46,15 @@ public class MainActivity extends AppCompatActivity {
     private String validity;
     private TextView mResultTextView;
 
-    Bitmap bitmap ;
+    Bitmap bitmap;
 
-    public final static int QRcodeWidth = 500 ;
-    private boolean paymentSuccessful= true;
+    public final static int QRcodeWidth = 500;
+    private boolean paymentSuccessful = true;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +71,9 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent, BARCODE_READER_REQUEST_CODE);
             }
         });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -64,10 +84,46 @@ public class MainActivity extends AppCompatActivity {
         TextView tv_amt = (TextView) findViewById(R.id.tv_amt);
         TextView tv_validity = (TextView) findViewById(R.id.tv_validity);
         Button btn_pay = (Button) findViewById(R.id.btn_pay);
-        int min = 100000;     int max=999999;
+        int min = 100000;
+        int max = 999999;
 
-        final int transaction_id =new Random().nextInt(max-min+1)+min;
+        final int transaction_id = new Random().nextInt(max - min + 1) + min;
         final String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+
+        Runnable runnable = new Runnable() {
+            public void run() {
+                //DynamoDB calls go here
+                CognitoCachingCredentialsProvider credentialsProvider = new CognitoCachingCredentialsProvider(
+                        getApplicationContext(),
+                        "us-west-2:6a90e3bc-32d9-4eb3-83c1-0d19aa5906fa", // Identity Pool ID
+                        Regions.US_WEST_2 // Region
+                );
+
+
+                AmazonDynamoDBClient ddbClient = Region.getRegion(Regions.US_WEST_2) // CRUCIAL
+
+                        .createClient(
+                                AmazonDynamoDBClient.class,
+                                credentialsProvider,
+                                new ClientConfiguration()
+                        );
+
+                DynamoDBMapper mapper = new DynamoDBMapper(ddbClient);
+
+                TicketDetailsDb tkt = new TicketDetailsDb();
+                String trans_id = transaction_id + "";
+                tkt.setTrans_id(trans_id);
+                tkt.setTime_stamp("yess");
+                tkt.setValidity(100);
+                tkt.setPenalty(121212);
+
+                mapper.save(tkt);
+            }
+        };
+
+
+        Thread mythread = new Thread(runnable);
+        mythread.start();
 
         if (requestCode == BARCODE_READER_REQUEST_CODE) {
             if (resultCode == CommonStatusCodes.SUCCESS) {
@@ -77,28 +133,28 @@ public class MainActivity extends AppCompatActivity {
                     Barcode barcode = data.getParcelableExtra(BarcodeCaptureActivity.BarcodeObject);
                     Point[] p = barcode.cornerPoints;
                     String qr_code_data = barcode.displayValue;
-                    String[] qr_code_data_parameters =  qr_code_data.split("&&");
+                    String[] qr_code_data_parameters = qr_code_data.split("&&");
 
                     service = qr_code_data_parameters[0];
                     account_no = qr_code_data_parameters[1];
                     amount = qr_code_data_parameters[2];
                     validity = qr_code_data_parameters[3];
 
-                    tv_service.setText(" Service Name                   : "+service);
-                    tv_accno.setText(  " Account Number                 : "+account_no);
-                    tv_amt.setText(    " Amount of Ticket (in Rs)       : " + amount);
+                    tv_service.setText(" Service Name                   : " + service);
+                    tv_accno.setText(" Account Number                 : " + account_no);
+                    tv_amt.setText(" Amount of Ticket (in Rs)       : " + amount);
                     tv_validity.setText(" Validity of Ticket (in hrs)    : " + validity);
                     btn_pay.setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View view) {
 
 
-                            if(paymentSuccessful) {
-                                String encode_ticket = service+"&&"+account_no+"&&"+amount+"&&"+validity+"&&"+transaction_id+"&&"+timeStamp;
+                            if (paymentSuccessful) {
+                                String encode_ticket = service + "&&" + account_no + "&&" + amount + "&&" + validity + "&&" + transaction_id + "&&" + timeStamp;
 
                                 try {
                                     bitmap = TextToImageEncode(encode_ticket);
-                                    ImageView imageView = (ImageView)findViewById(R.id.imageView);
+                                    ImageView imageView = (ImageView) findViewById(R.id.imageView);
                                     imageView.setImageBitmap(bitmap);
 
                                 } catch (WriterException e) {
@@ -108,7 +164,7 @@ public class MainActivity extends AppCompatActivity {
                             }
                         }
                     });
-                   // mResultTextView.setText(barcode.displayValue);
+                    // mResultTextView.setText(barcode.displayValue);
 
                 } else setContentView(R.layout.activity_main);
             } else Log.e(LOG_TAG, String.format(getString(R.string.barcode_error_format),
@@ -122,7 +178,7 @@ public class MainActivity extends AppCompatActivity {
         try {
             bitMatrix = new MultiFormatWriter().encode(
                     Value,
-                    BarcodeFormat.DATA_MATRIX.QR_CODE,
+                    BarcodeFormat.QR_CODE,
                     QRcodeWidth, QRcodeWidth, null
             );
 
@@ -142,7 +198,7 @@ public class MainActivity extends AppCompatActivity {
             for (int x = 0; x < bitMatrixWidth; x++) {
 
                 pixels[offset + x] = bitMatrix.get(x, y) ?
-                        ContextCompat.getColor(MainActivity.this, android.R.color.black): ContextCompat.getColor(MainActivity.this, android.R.color.white);
+                        ContextCompat.getColor(MainActivity.this, android.R.color.black) : ContextCompat.getColor(MainActivity.this, android.R.color.white);
 
             }
         }
@@ -150,5 +206,41 @@ public class MainActivity extends AppCompatActivity {
 
         bitmap.setPixels(pixels, 0, 500, 0, 0, bitMatrixWidth, bitMatrixHeight);
         return bitmap;
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
---
+++ app/src/main/java/com/varvet/barcodereadersample/TicketDetailsDb.java
@@ -0,0 +1,55 @@
+package com.varvet.barcodereadersample;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.*;
+/**
+ * Created by Abhilash on 06-03-2017.
+ */
+
+@DynamoDBTable(tableName = "TicketmDetailsDb")
+public class TicketDetailsDb {
+    private String trans_id;
+    private int validity;
+    private String service;
+    private String time_stamp;
+    private int penalty;
+    private Long key;
+
+
+
+    @DynamoDBHashKey(attributeName = "Trans_id")
+    public String getTrans_id() {
+        return trans_id;
+    }
+
+    public void setTrans_id(String trans_id) {
+        this.trans_id = trans_id;
+    }
+
+
+    @DynamoDBAttribute(attributeName = "Time_stamp")
+    public String getTime_stamp() {
+        return time_stamp;
+    }
+
+    public void setTime_stamp(String time_stamp) {
+        this.time_stamp = time_stamp;
+    }
+
+    @DynamoDBAttribute(attributeName = "Validity")
+    public int getValidity() {
+        return validity;
+    }
+
+    public void setValidity(int validity) {
+        this.validity = validity;
+    }
+
+    @DynamoDBAttribute(attributeName = "Penalty")
+    public int getPenalty() {
+        return penalty;
+    }
+
+    public void setPenalty(int penalty) {
+        this.penalty = penalty;
+    }
+
+}
