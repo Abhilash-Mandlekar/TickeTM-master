8676ea315c31cef0e48ec37bfe16fc01fe464801
ChinmayDegwekar <csdegwekar@gmail.com>
2017-04-04 12:46:08 +0530

    Transaction in main class

--- app/src/main/java/com/varvet/barcodereadersample/MainActivity.java
+++ app/src/main/java/com/varvet/barcodereadersample/MainActivity.java
@@ -26,10 +26,16 @@ import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
+import com.paytm.pgsdk.PaytmMerchant;
+import com.paytm.pgsdk.PaytmOrder;
+import com.paytm.pgsdk.PaytmPGService;
+import com.paytm.pgsdk.PaytmPaymentTransactionCallback;
 import com.varvet.barcodereadersample.barcode.BarcodeCaptureActivity;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 import com.amazonaws.auth.CognitoCachingCredentialsProvider;
 import com.amazonaws.regions.Regions;
@@ -155,11 +161,11 @@ public class MainActivity extends AppCompatActivity {
                         public void onClick(View view) {
 
                             MerchantActivity mMerchantActivity = new MerchantActivity();
-                            mMerchantActivity.onStart();
-                            mMerchantActivity.onStartTransaction(amount);
+                           // mMerchantActivity.onStart();
+                            onStartTransaction(amount);
 
-                            Intent pay_intent = new Intent(MainActivity.this, MerchantActivity.class);
-                            MainActivity.this.startActivity(pay_intent);
+//                            Intent pay_intent = new Intent(MainActivity.this, MerchantActivity.class);
+//                            MainActivity.this.startActivity(pay_intent);
 
 
                             if(responseCode.equals("01")&&responseMSG.equals("Txn Successful."))
@@ -261,4 +267,109 @@ public class MainActivity extends AppCompatActivity {
         AppIndex.AppIndexApi.end(client, getIndexApiAction());
         client.disconnect();
     }
+
+
+
+    //-------------------------------------------------------------------------------------------------------------------------
+
+
+    public void onStartTransaction(String amount) {//VIEW PARAMETER REMOVED
+        PaytmPGService Service = PaytmPGService.getStagingService();
+        Map<String, String> paramMap = new HashMap<String, String>();
+
+        // these are mandatory parameters
+        Random r = new Random(System.currentTimeMillis());
+        String orderId = "ORDERDD" + (1 + r.nextInt(2)) * 10000
+                + r.nextInt(10000);
+
+
+        paramMap.put("ORDER_ID", orderId);
+        paramMap.put("MID", "WorldP64425807474247");
+        paramMap.put("CUST_ID", "CUST23657");
+        paramMap.put("CHANNEL_ID", "WAP");
+        paramMap.put("INDUSTRY_TYPE_ID", "Retail");
+        paramMap.put("WEBSITE", "worldpressplg");
+        //paramMap.put("TXN_AMOUNT", ((EditText) findViewById(R.id.transaction_amount)).getText().toString());
+        paramMap.put("TXN_AMOUNT", amount);
+
+        paramMap.put("THEME", "merchant");
+        paramMap.put("EMAIL", "abhi@gmail.com");
+        paramMap.put("MOBILE_NO","123");
+        PaytmOrder Order = new PaytmOrder(paramMap);
+
+        PaytmMerchant Merchant = new PaytmMerchant(
+                "https://pguat.paytm.com/paytmchecksum/paytmCheckSumGenerator.jsp",
+                "https://pguat.paytm.com/paytmchecksum/paytmCheckSumVerify.jsp");
+
+        Service.initialize(Order, Merchant, null);
+
+        Service.startPaymentTransaction(this, true, true,
+                new PaytmPaymentTransactionCallback() {
+                    @Override
+                    public void someUIErrorOccurred(String inErrorMessage) {
+                        // Some UI Error Occurred in Payment Gateway Activity.
+                        // // This may be due to initialization of views in
+                        // Payment Gateway Activity or may be due to //
+                        // initialization of webview. // Error Message details
+                        // the error occurred.
+                    }
+
+                    @Override
+                    public void onTransactionSuccess(Bundle inResponse) {
+                        // After successful transaction this method gets called.
+                        // // Response bundle contains the merchant response
+                        // parameters.
+                        Log.d("LOG", "Payment Transaction is successful " + inResponse);
+                        Toast.makeText(getApplicationContext(), "Payment Transaction is successful ", Toast.LENGTH_LONG).show();
+                        MainActivity.date = inResponse.getString("TXNDATE");
+                        MainActivity.responseCode = inResponse.getString("RESPCODE");
+                        MainActivity.responseMSG = inResponse.getString("RESPMSG");
+                        MainActivity.txnID = inResponse.getString("TXNID");
+                    }
+
+                    @Override
+                    public void onTransactionFailure(String inErrorMessage,
+                                                     Bundle inResponse) {
+                        // This method gets called if transaction failed. //
+                        // Here in this case transaction is completed, but with
+                        // a failure. // Error Message describes the reason for
+                        // failure. // Response bundle contains the merchant
+                        // response parameters.
+                        Log.d("LOG", "Payment Transaction Failed " + inErrorMessage);
+                        Toast.makeText(getBaseContext(), "Payment Transaction Failed ", Toast.LENGTH_LONG).show();
+                    }
+
+                    @Override
+                    public void networkNotAvailable() { // If network is not
+                        // available, then this
+                        // method gets called.
+                    }
+
+                    @Override
+                    public void clientAuthenticationFailed(String inErrorMessage) {
+                        // This method gets called if client authentication
+                        // failed. // Failure may be due to following reasons //
+                        // 1. Server error or downtime. // 2. Server unable to
+                        // generate checksum or checksum response is not in
+                        // proper format. // 3. Server failed to authenticate
+                        // that client. That is value of payt_STATUS is 2. //
+                        // Error Message describes the reason for failure.
+                    }
+
+                    @Override
+                    public void onErrorLoadingWebPage(int iniErrorCode,
+                                                      String inErrorMessage, String inFailingUrl) {
+
+                    }
+
+                    // had to be added: NOTE
+                    @Override
+                    public void onBackPressedCancelTransaction() {
+                        // TODO Auto-generated method stub
+                    }
+
+                });
+    }
+
+
 }
